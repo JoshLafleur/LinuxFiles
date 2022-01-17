906426b002c8525dfb874ad0a62cf00d71a4c7a4
JoshLafleur <77811114+JoshLafleur@users.noreply.github.com>
2022-01-16 20:52:42 -0500

    Update LinuxFilesInstall.sh

--- scripts/LinuxFilesInstall.sh
+++ scripts/LinuxFilesInstall.sh
@@ -1,4 +1,4 @@
 echo ".cfg" >> .gitignore
-git clone --bare /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME  $HOME/.cfg
+git clone --bare <remote-git-repo-url> $HOME/.cfg
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout GentooLaptop
