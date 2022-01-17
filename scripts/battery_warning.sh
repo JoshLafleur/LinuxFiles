684a62ffb815077ff39509498dd672e5abf79d76
GentooLaptop <josh.lafleur@outlook.com>
2022-01-17 09:09:53 -0500

    .

--- scripts/battery_warning.sh
+++ scripts/battery_warning.sh
@@ -2,7 +2,7 @@
 bat_files="/sys/class/power_supply/BAT0"
 bat_status=$(cat ${bat_files}/status)
 capacity=$(cat "${bat_files}/capacity")
-if [[ ${bat_status}=="Discharging" && ${capacity} -ge 20 ]]; then
+if [[ ${bat_status}=="Discharging" && ${capacity} -le 20 ]]; then
     echo "Battery alert - ${capacity}%"
     notify-send \
         "Low battery" \
