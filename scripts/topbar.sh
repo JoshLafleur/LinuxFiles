4d2019bb16a18a21013fe726d2baa7c62ab14c1e
GentooLaptop <josh.lafleur@outlook.com>
2022-01-18 21:48:28 -0500

    m

--- scripts/topbar.sh
+++ scripts/topbar.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+delim='|'
 i3status | while :
 do
-  gen=''
-  pck=''
-  eta=''
+  #gen=''
+  #pck=''
+  #eta=''
+  out=""
   gen=$(sudo genlop -c -n )
   pck=$(grep \* <<< "$gen")
   cur=$(grep Currently <<< $gen)
   eta=$(grep ETA <<< $gen)
   if [[ $pck != '' ]]; then 
-     echo "$cur -> ${pck:3} ${eta:7}" || exit 1
-   else
-     echo "" || exit 1
+    out+="  $cur -> ${pck:3} ${eta:7}" 
   fi
+  #dmsg=$(dmesg | tail -n1)
+  #if [[ $out != '' ]]; then
+  #  out+=" $delim"
+  #fi
+  #out+="  $dmsg"
+  #log=$(tail -n1 /var/log/syslog)
+  #if [[ $out != '' ]]; then
+  #  out+=" $delim"
+  #fi
+  #out+="   $log"
+  echo "$out" && sleep 1 || exit 1
 done
