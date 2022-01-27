e9a5518a57f1b1b09f3072a116f7415ba2d48f42
GentooLaptop <josh.lafleur@outlook.com>
2022-01-27 08:44:42 -0500

    Zoom config settings

--- scripts/battery_warning.sh
+++ scripts/battery_warning.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 bat_files="/sys/class/power_supply/BAT0"
-bat_status=$(/bin/cat ${bat_files}/status)
+bat_status=$(/bin/cat "${bat_files}/status")
 capacity=$(/bin/cat "${bat_files}/capacity")
-if [[ ${bat_status}=="Discharging" && ${capacity} -le 20 && ${capacity} -gt 10 ]]; then
+if [[ ${bat_status} == "Discharging" && ${capacity} -le 20 && ${capacity} -gt 10 ]]; then
     env DISPLAY=:0.0 sudo -u josh /usr/bin/libnotify-notify-send \
         -t 5000 "Low battery" \
         "Only ${capacity}% battery remaining"
-elif [[ ${bat_status}=="Discharging" && ${capacity} -le 10 ]]; then
+elif [[ ${bat_status} == "Discharging" && ${capacity} -le 10 ]]; then
     env DISPLAY=:0.0 sudo -u josh /usr/bin/libnotify-notify-send \
         -t 60000 -u critical "Low battery" \
         "Only ${capacity}% battery remaining"
